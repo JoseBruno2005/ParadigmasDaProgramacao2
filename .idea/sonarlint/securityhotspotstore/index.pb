14c278d07c53486a1695a3337c686db3b957fd93
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2024-09-12 14:13:23 -0300

    Atualização carrinho Estruturada

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/main/java/projetoCalibracao/domain/Calibrador.java,6\9\6900dddf07944902c0eac95bde14de3dcb99ec16
+^
+.src/main/java/projetoCalibracao/main/Main.java,6\1\61a8bd427f9c20a3bf451b3b67f68efa6422f37a
+b
+2src/main/java/projetoCarrinho/domain/Carrinho.java,d\b\dbb0716328bb355370e2a235530a227b99244297
+[
++src/main/java/projetoCarrinho/main/App.java,4\2\421b7d2ac7d6638a2df3f8a1c9533b155383153f
+a
+1src/main/java/projetoCarrinho/domain/Produto.java,2\6\2682fe36cd96f049e088920c5d5062c564b8f7f9
--- src/main/java/projetoCarrinho/main/App.java
+++ src/main/java/projetoCarrinho/main/App.java
@@ -1,24 +1,120 @@
 package projetoCarrinho.main;
 
-import projetoCarrinho.domain.Carrinho;
-import projetoCarrinho.domain.Produto;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 
-    public static void main(String[] args) {
+    // Variáveis globais
+    static List<Map<String, Object>> listaDeProdutos = new ArrayList<>();
+    static float acrescimoTotal = 0;
+    static float descontoTotal = 0;
+    static float valorTotal = 0;
 
-        var carrinho = new Carrinho();
-
-        carrinho.inserirItens(new Produto("Cigarro", "001", "Mata", 10f));
-        carrinho.inserirItens(new Produto("Maconha", "002", "Chapa", 15f));
-
-        carrinho.descontoNoProduto("001", 2.0f);
-
-        carrinho.setAcrescimoTotal(10);
-        carrinho.setDescontoTotal(4);
-
-        carrinho.finalizarVenda();
-
+    //criar produto
+    public static Map<String, Object> criarProduto(String nome, String codigo, String descricao, float preco) {
+        Map<String, Object> produto = new HashMap<>();
+        produto.put("nome", nome);
+        produto.put("codigo", codigo);
+        produto.put("descricao", descricao);
+        produto.put("preco", preco);
+        produto.put("acrescimo", 0f);
+        produto.put("desconto", 0f);
+        return produto;
     }
 
+    //inserir item na lista
+    public static String inserirItens(Map<String, Object> produto) {
+        listaDeProdutos.add(produto);
+        return produto.get("nome") + " adicionado!";
+    }
+
+    //acréscimo em um produto
+    public static void acrescimoNoProduto(String codigo, float acrescimo) {
+        for (Map<String, Object> produto : listaDeProdutos) {
+            if (produto.get("codigo").equals(codigo)) {
+                float precoAtualizado = (float) produto.get("preco") + acrescimo;
+                produto.put("preco", precoAtualizado);
+                float acrescimoAtualizado = (float) produto.get("acrescimo") + acrescimo;
+                produto.put("acrescimo", acrescimoAtualizado);
+                System.out.println("Valor de " + produto.get("nome") + " Atualizado para " + precoAtualizado);
+            }
+        }
+    }
+
+    //desconto em um produto
+    public static void descontoNoProduto(String codigo, float desconto) {
+        for (Map<String, Object> produto : listaDeProdutos) {
+            if (produto.get("codigo").equals(codigo) && (float) produto.get("preco") > desconto) {
+                float precoAtualizado = (float) produto.get("preco") - desconto;
+                produto.put("preco", precoAtualizado);
+                float descontoAtualizado = (float) produto.get("desconto") + desconto;
+                produto.put("desconto", descontoAtualizado);
+                System.out.println("Valor de " + produto.get("nome") + " Com desconto é de " + precoAtualizado);
+            }
+        }
+    }
+
+    //acréscimo total
+    public static void setAcrescimoTotal(float acrescimoTotal) {
+        float valorParaCadaProduto = acrescimoTotal / listaDeProdutos.size();
+
+        for (Map<String, Object> produto : listaDeProdutos) {
+            float precoAtualizado = (float) produto.get("preco") + valorParaCadaProduto;
+            produto.put("preco", precoAtualizado);
+            float acrescimoAtualizado = (float) produto.get("acrescimo") + valorParaCadaProduto;
+            produto.put("acrescimo", acrescimoAtualizado);
+        }
+    }
+
+    //desconto total
+    public static void setDescontoTotal(float descontoTotal) {
+        float valorParaCadaProduto = descontoTotal / listaDeProdutos.size();
+
+        for (Map<String, Object> produto : listaDeProdutos) {
+            if ((float) produto.get("preco") > valorParaCadaProduto) {
+                float precoAtualizado = (float) produto.get("preco") - valorParaCadaProduto;
+                produto.put("preco", precoAtualizado);
+                float descontoAtualizado = (float) produto.get("desconto") + valorParaCadaProduto;
+                produto.put("desconto", descontoAtualizado);
+            }
+        }
+    }
+
+    // Função para finalizar a venda
+    public static void finalizarVenda() {
+        for (Map<String, Object> produto : listaDeProdutos) {
+            acrescimoTotal += (float) produto.get("acrescimo");
+            descontoTotal += (float) produto.get("desconto");
+            valorTotal += (float) produto.get("preco");
+            System.out.println("Nome: " + produto.get("nome"));
+            System.out.println("Codigo: " + produto.get("codigo"));
+            System.out.println("Descrição: " + produto.get("descricao"));
+            System.out.println("Acrescimo: " + produto.get("acrescimo"));
+            System.out.println("Desconto: " + produto.get("desconto"));
+            System.out.println("Preço: " + produto.get("preco"));
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        }
+        System.out.println("Acrescimo Total: " + acrescimoTotal);
+        System.out.println("Desconto Total: " + descontoTotal);
+        System.out.println("Valor Total: " + valorTotal);
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> p1 = criarProduto("Produto1", "001", "Descrição do Produto 1", 10);
+        Map<String, Object> p2 = criarProduto("Produto2", "002", "Descrição do Produto 2", 5);
+
+        System.out.println(inserirItens(p1));
+        System.out.println(inserirItens(p2));
+
+        acrescimoNoProduto("001", 10);
+        descontoNoProduto("002", 20);
+
+        setAcrescimoTotal(30);
+        setDescontoTotal(40);
+
+        finalizarVenda();
+    }
 }
