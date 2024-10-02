2048cf5047bc9f3a36437276a7c1db2c6234fd53
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2024-10-02 19:18:40 -0300

    Calibrador coom therads

---
+++ src/main/java/projetoCalibradorThreads/domain/CalibradorComThreads.java
@@ -0,0 +1,25 @@
+package projetoCalibradorThreads.domain;
+
+public class CalibradorComThreads extends Thread{
+
+    public static int valorCalibracao(String linha) {
+        Character primeiroCaractere = null;
+        Character ultimoCaractere = null;
+
+
+        for (char c : linha.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (primeiroCaractere == null) {
+                    primeiroCaractere = c;
+                }
+                ultimoCaractere = c;
+            }
+        }
+
+        if (primeiroCaractere != null) {
+            return Integer.parseInt(primeiroCaractere.toString() + ultimoCaractere.toString());
+        } else {
+            return 0;
+        }
+    }
+}
